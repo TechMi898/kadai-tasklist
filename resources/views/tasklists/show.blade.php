eeff609d59c3a30d66a39eadbd04e3b8ba9af815
tech.mi <techmi898@gmail.com>
2018-06-18 20:54:19 +0000

    CRUD tasklists

---
+++ resources/views/tasklists/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
+    
+    <p>{{ $tasklist->content }}</p>
+    
+    {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => '$tasklist->id']) !!}
+
+    {!! Form::model($tasklist, ['route' => ['tesklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
+    
+        {!! Form::submit('削除') !!}
+        
+    {!! Form::close() !!}
+
+@endsection
