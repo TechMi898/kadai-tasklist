63b73404b5577b21463b2ff3ae570d99af348971
tech.mi <techmi898@gmail.com>
2018-06-22 06:45:28 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,6 +9,13 @@
         {!! Form::label('content', 'タスク：') !!}
         {!! Form::text('content') !!}
         
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::select('status', [
+            '未開始' => '未開始',
+            '実行中' => '実行中',
+            '繰延' => '繰延',
+            '完了' => '完了']) !!}
+        
         {!! Form::submit('追加') !!}
         
     {!! Form::close() !!}
