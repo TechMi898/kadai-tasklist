c895b46b52068edd45654a909784f5586a1d8649
tech.mi <techmi898@gmail.com>
2018-07-06 05:49:21 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
